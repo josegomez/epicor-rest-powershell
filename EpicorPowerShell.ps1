ca8e68dab071de860c28678e386615466a87174c
Jose C Gomez <jose@josecgomez.com>
2019-03-14 13:06:21 -0400

    Added Powershell Example

---
+++ EpicorPowerShell.ps1
@@ -0,0 +1,20 @@
+#Epicor user name
+$user = "epicor"
+#Epicor Password
+$pass= "epicor"
+
+
+#BAQ End point URL
+$uri = "https://YourEpicorServer.TLD/EpicorProduction/api/v1/BaqSvc/zCustomer01/"
+
+#Create encoded password
+$secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
+
+#Create Credential
+$cred = New-Object System.Management.Automation.PSCredential ($pass, $secpasswd)
+
+#Rest Call
+$data = Invoke-RestMethod $uri -Method Get -Credential $cred -ContentType "Applicatin/Json"
+
+#Dump results as CSV
+$data.value | Export-Csv -Path './test.csv' -NoTypeInformation
